4d7d60325746ec8d82e3df3224177cca712d6c6f
Sirawich Phasuk <sirawich.phas@mail.kmutt.ac.th>
2025-10-27 18:19:29 +0700

    081 [add] fix store and fetch api for review

--- src/libs/MovieApi.js
+++ src/libs/MovieApi.js
@@ -1,7 +1,13 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
+// Get all movies
 export async function getAllMovies() {
-    const response = await fetch(`${API_URL}/movies`);
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${API_URL}/movies`);
+  return await response.json();
+}
+
+// Get movie by ID
+export async function getMovieById(id) {
+  const response = await fetch(`${API_URL}/movies/${id}`);
+  return await response.json();
 }
